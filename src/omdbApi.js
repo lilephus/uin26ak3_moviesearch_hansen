76f7bd95c3942f8e159e5ab61f14848673bd30ae
Ephantuh <34236501+lilephus@users.noreply.github.com>
2026-03-15 00:42:05 +0100

    Tester api v3

--- src/omdbApi.js
+++ src/omdbApi.js
@@ -7,9 +7,3 @@ export async function searchMovies(title) {
   const data = await res.json();
   return data.Search || [];
 }
-  const response = await fetch(
-    `https://www.omdbapi.com/?apikey=${API_KEY}&s=${encodeURIComponent(searchTerm)}`
-  );
-  const data = await response.json();
-  return data.Search || [];
-}
