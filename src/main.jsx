0faed1e253d8d69a897ab02e39f5767a5240ce67
Ephantuh <34236501+lilephus@users.noreply.github.com>
2026-03-15 00:40:12 +0100

    Tester main.jsx og api.js

--- src/main.jsx
+++ src/main.jsx
@@ -3,3 +3,9 @@ import ReactDOM from "react-dom/client";
 import App from "./App,jsx";
 import "./index.css";
 
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(
+  <StrictMode>
+    <App />
+  </StrictMode>
+);  
--- src/omdbApi.js
+++ src/omdbApi.js
@@ -1,1 +1,9 @@
 const API_KEY = "684ca942";
+
+export async function fetchMovies(searchTerm) {
+  const response = await fetch(
+    `https://www.omdbapi.com/?apikey=${API_KEY}&s=${encodeURIComponent(searchTerm)}`
+  );
+  const data = await response.json();
+  return data.Search || [];
+}
