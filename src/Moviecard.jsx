bc98fbba54452659bfdbbf630a77fa0df9d19067
Ephantuh <34236501+lilephus@users.noreply.github.com>
2026-03-15 01:30:55 +0100

    fixed moviepage, index og movie card bytter posterurl til poster

--- src/Moviecard.jsx
+++ src/Moviecard.jsx
@@ -1,21 +1,21 @@
 import {link} from "react-router-dom";
 //impoterer placeholderbilde for filmer uten poster
-import Placeholder from "./assets/placeholder.png";
+import Placeholder from "./assets/Placeholder.png";
 
 function MovieCard({ movie }) {
 //lager slug av tittelen så den kan bruker i urln
   const slug = movie.Title.toLowerCase().replace(/\s+/g, "-");
 
 //placejhlder bilder brukes her hvis det ikke er noe bilde til apiet
-  let posterUrl = movie.Poster;
-  if (posterUrl === "N/A") {
-    posterUrl = Placeholder;
+  let poster = movie.Poster;
+  if (poster === "N/A") {
+    poster = Placeholder;
   }
 
   return (
     <article className="movie-card">
       <Link to={`/movies/${slug}`}>
-        <img src={posterUrl} alt={movie.Title} />
+        <img src={poster} alt={movie.Title} />
         <h3>{movie.Title}</h3>
         <p>{movie.Year}</p>
       </Link>
