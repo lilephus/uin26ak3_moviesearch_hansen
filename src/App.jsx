e9b8ae528d1dea04f2a86c36d44428380f83ac2d
Ephantuh <34236501+lilephus@users.noreply.github.com>
2026-03-15 01:02:51 +0100

    endret app.jsx lagt til routing

--- src/App.jsx
+++ src/App.jsx
@@ -5,9 +5,14 @@ import MoviePage from "./MoviePage";
 function App() {
   return (
     <BrowserRouter>
+    <main>
       <Routes>
-        <Route path="/movies/:slug" element={<MoviePage />} />
+        <Route path="/:movie" element={<MoviePage />} />
+        {/* <Route path="/movies/:slug" element={<MoviePage />} /> */}
       </Routes>
+    </main>
     </BrowserRouter>
   );
 }
+
+export default App;
