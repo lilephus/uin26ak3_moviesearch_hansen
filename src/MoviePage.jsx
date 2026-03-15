cb5b57e8d97625d569626a7f77d07729ee12897c
Ephantuh <34236501+lilephus@users.noreply.github.com>
2026-03-15 01:40:09 +0100

    fixet import i main jsx hvor jeg hadde brukt app,jsx isten for . og lagt til esport default som manglet

---
+++ src/MoviePage.jsx
@@ -0,0 +1,18 @@
+import { useParams } from "react-router-dom";
+
+function MoviePage() {
+  const { movie } = useParams();
+
+  //denne omgjør slug tilt tittel  for å kunne lage url av tittelen og vise den på siden
+  const title = movie.replace(/-/g, " ");
+
+    return (
+        <main>
+            <header>
+                <h1>{title}</h1>
+            </header>
+        </main>
+    );
+}
+
+export default MoviePage;
--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,7 @@
 import { StrictMode } from "react";
 // import ReactDOM from "react-dom/client";
 import { createRoot } from "react-dom/client";
-import App from "./App,jsx";
+import App from "./App.jsx";
 import "./index.css";
 
 // const root = ReactDOM.createRoot(document.getElementById("root"));
