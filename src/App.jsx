20d9cf58d9b93724b0aa6745631ded3bf536b3ee
Ephantuh <34236501+lilephus@users.noreply.github.com>
2026-03-15 01:20:49 +0100

    prøvd å fixe jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,17 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import SearchResults from "./SearchResults";
 import MoviePage from "./MoviePage";
+// import SearchResults from "./SearchResults";
 
 
+//brukers til routing her
 function App() {
   return (
     <BrowserRouter>
     <main>
       <Routes>
         <Route path="/:movie" element={<MoviePage />} />
+        <Route path="/" element={<SearchResults />} />
         {/* <Route path="/movies/:slug" element={<MoviePage />} /> */}
       </Routes>
     </main>
