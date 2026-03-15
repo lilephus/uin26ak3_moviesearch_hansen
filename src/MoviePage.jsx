cdc798c32a83feef273f6403186c4903af16ec4c
Ephantuh <34236501+lilephus@users.noreply.github.com>
2026-03-15 14:26:17 +0100

    fixed moviepage jsx fil med å legge til mer movie info

--- src/MoviePage.jsx
+++ src/MoviePage.jsx
@@ -27,16 +27,20 @@ function MoviePage() {
                 <h1>{title}</h1>
             </header>
 
-            <img src={movieData.Poster} alt={movieData.Title} />
-            <p><strong>Released:</strong> {movieData.Released}</p>
-            <p><strong>Genre:</strong> {movieData.Genre}</p>
-            <p><strong>Runtime:</strong> {movieData.Runtime}</p>
-            <p><strong>Director:</strong> {movieData.Director}</p>
-            <p><strong>Actors:</strong> {movieData.Actors}</p>
-            <p><strong>Plot:</strong> {movieData.Plot}</p>
+            {movieData.Poster !== "N/A" && (
+                <img src={movieData.Poster} alt={movieData.Title} />
+            )}
+
+            {/* <img src={movieData.Poster} alt={movieData.Title} || "NA" /> */}
+            <p><strong>Released:</strong> {movieData.Released || "NA"}</p>
+            <p><strong>Genre:</strong> {movieData.Genre || "NA"}</p>
+            <p><strong>Runtime:</strong> {movieData.Runtime || "NA"}</p>
+            <p><strong>Director:</strong> {movieData.Director || "NA"}</p>
+            <p><strong>Actors:</strong> {movieData.Actors || "NA"}</p>
+            {/* <p><strong>Plot:</strong> {movieData.Plot || "NA"}</p> */}
             <p><strong>IMDB Rating:</strong> {movieData.imdbRating}</p>
-        <p>{movieData.Plot}</p>
-        
+        <p>{movieData.Plot || "No description available."}</p>
+
         </main>
     );
 }
